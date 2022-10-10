88a04a55d0985cf61cb5155ffa2d9244b77951c9
Byeon Seongun <kamyu1537@gmail.com>
2022-10-11 04:09:58 +0900

    EAC Kick Handler 추가

---
+++ src/handlers/player-eac-kick-handler.ts
@@ -0,0 +1,19 @@
+import type { IMessageHandler, RconMessageType } from '../lib/webrcon/types';
+
+interface IEacKickData {
+  displayName: string;
+  steamId: string;
+  reason: string;
+}
+
+class PlayerEacKickHandler implements IMessageHandler<IEacKickData> {
+  type: RconMessageType = 'Generic';
+  pattern = /\[EAC\] Kicking (.*) \/ (7656[0-9]{13}) (.*)/g;
+  dataKeys = ['displayName', 'steamId', 'reason'];
+
+  handle(data: IEacKickData): void {
+    console.log('player eac kicked:', data);
+  }
+}
+
+export default PlayerEacKickHandler;

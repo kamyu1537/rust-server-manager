c51ef7de8967c077ea782b92f5fccf354d88a510
Byeon Seongun <kamyu1537@gmail.com>
2022-10-11 04:41:22 +0900

    저장이 되면 자동으로 백업 명령어를 실행하도록 변경

--- src/handlers/save-handler.ts
+++ src/handlers/save-handler.ts
@@ -1,11 +1,13 @@
+import WebRcon from '../lib/webrcon';
 import type { IMessageHandler, RconMessageType } from '../lib/webrcon/types';
 
 class SaveHandler implements IMessageHandler {
   type: RconMessageType = 'Generic';
   pattern = 'Saving complete';
 
-  handle(): void {
-    console.info('saved!');
+  handle(_: unknown, webrcon: WebRcon): void {
+    console.info('save detected, run backup command');
+    webrcon.commandAsync('server.backup').then().catch(console.error);
   }
 }
 

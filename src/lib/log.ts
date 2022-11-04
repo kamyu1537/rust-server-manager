e52ae3164ba0f3f87b29cfb9e856584b58875788
Byeon Seongun <kamyu1537@gmail.com>
2022-11-04 19:47:02 +0900

    로그에 luxon 을 이용하여 타임존 설정

--- src/lib/log.ts
+++ src/lib/log.ts
@@ -1,21 +1,22 @@
 import fs from 'fs';
+import { DateTime } from 'luxon';
 
 export function appendLog(log: string, name = 'log') {
-  const date = new Date().toISOString();
-  const suffix = '-' + date.slice(0, 10) + '.log';
+  const date = DateTime.local({ zone: process.env.TZ });
+  const prefix = date.toFormat('yyyy-MM-dd') + '_';
 
   if (!fs.existsSync('./log')) {
     fs.mkdirSync('./log');
   }
 
-  const fileName = name + suffix;
-  const appendLine = `[${date}] (${name}) ${log}`;
+  const fileName = prefix + name + '.log';
+  const appendLine = `[${date.toString()}] (${name}) ${log}`;
   console.info(appendLine);
 
   fs.appendFileSync('./log/' + fileName, appendLine + '\n', {});
 
-  // 모든 로그가 log- 파일에 모입니다.
+  // console 로그를 제외한 모든 로그가 all 파일에 모입니다.
   if (name !== 'log' && name !== 'console') {
-    fs.appendFileSync('./log/log' + suffix, appendLine + '\n', {});
+    fs.appendFileSync('./log/' + prefix + '_all.log', appendLine + '\n', {});
   }
 }
